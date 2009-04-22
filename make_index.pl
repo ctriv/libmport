594696cbe33c5eee2902c0400415eb196d5b9206
Chris Reinhardt <ctriv@midnightbsd.org>
2009-04-22 18:55:13 -0400

    Add docs to make_index.pl.

--- make_index.pl
+++ make_index.pl
@@ -9,6 +9,31 @@ use Getopt::Std;
 use File::Path;
 use YAML qw(LoadFile);
 
+=head1 NAME
+
+bless_run.pl -- Promote A Run
+
+=head1 SYNOPSIS
+
+ bless_run.pl -r RUN -m MIRRORLIST -f FTPROOT -a ALIASLIST
+ 
+=head1 DESCRIPTION
+
+A run in magus can be blessed into a index that is used by the mport package
+system.  This index contains a list of availible packages, a list of
+aliases, and a list of mirrors.  The index is an L<sqlite3> database file that
+is compressed with L<bzip2>.
+
+This script will create the index file and copy the bundle files into the 
+given ftp root.  The ftp root is organized as such:
+
+  $FTPROOT/$run->arch/$run->osversion/
+        index.db.bz2
+        bundle1.mport
+        bundle2.mport
+        ...
+=cut
+
 sub main {
   my %opts;
   
@@ -31,10 +56,10 @@ sub main {
   build_packages_table(\%opts, $index, $run);
   build_aliases_table(\%opts, $index, $run);
   build_mirror_list(\%opts, $index, $run);
+  copy_bundle_files(\%opts, $index, $run);
   
-  finish_index(\%opts, $index, $run);  
-  copy_bundle_files(\%opts, $run);
-  
+  finish_index(\%opts, $index, 'index.db');  
+
   move_dirs(\%opts);
 }
 
@@ -53,11 +78,11 @@ sub make_db_file {
   
   my $dbh = DBI->connect("dbi:SQLite:dbname=$file","","", { RaiseError => 1 });
   
-  $dbh->do("CREATE TABLE packages (pkg text NOT NULL, version text NOT NULL, comment text NOT NULL, www text NOT NULL, bundlefile text NOT NULL");
+  $dbh->do("CREATE TABLE packages (pkg text NOT NULL, version text NOT NULL, comment text NOT NULL, www text NOT NULL, bundlefile text NOT NULL)");
   $dbh->do("CREATE UNIQUE INDEX packages_pkg ON packages (pkg)");
   
   $dbh->do("CREATE TABLE categories (pkg text NOT NULL, category text NOT NULL)");
-  $dbh->do("CREATE INDEX categories_pkg ON categories (pkg, category");
+  $dbh->do("CREATE INDEX categories_pkg ON categories (pkg, category)");
 
   $dbh->do("CREATE TABLE aliases (alias text NOT NULL, pkg text NOT NULL)");
   $dbh->do("CREATE UNIQUE INDEX aliases_als ON aliases (alias)");
@@ -125,3 +150,39 @@ sub build_mirror_list {
   $sth->finish;
   $index->commit;
 }
+
+
+sub finish_index {
+  my ($opts, $index, $file) = @_;
+  
+  $index->disconnect;
+
+  my $cmnd = "bzip2 $opts->{f}/$file";
+  
+  if (system($cmnd) != 0) {
+    die "$cmnd returned non-zero: $?\n";
+  }
+  
+  unlink("$opts->{f}/$file");
+}
+
+
+=head1 BUGS
+
+Many.  Contact ctriv@MidnightBSD.org.
+
+=head1 AUTHOR
+
+Chris Reinhardt <ctriv@midnightbsd.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2009 Chris Reinhardt
+All rights reserved.
+
+=head1 LICENSE
+
+This software is licensed under the 2-clause BSD license.
+
+=cut
+
